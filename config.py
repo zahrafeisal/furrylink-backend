c552eb850c0462e39f88d2f4e7451819c1fa9a55
zahrafeisal <zahrafeisal3@gmail.com>
2025-05-08 19:22:59 +0300

    configurations

--- config.py
+++ config.py
@@ -11,7 +11,13 @@ from dotenv import load_dotenv    # from python-dotenv library
 load_dotenv()   # load env variables
 
 app = Flask(__name__)
-CORS(app, methods=["GET", "POST", "PATCH", "DELETE"], allow_headers=["Content-Type", "Accept"])
+CORS(
+    app,
+    supports_credentials=True,
+    methods=["GET", "POST", "PATCH", "DELETE"],
+    allow_headers=["Content-Type", "Accept"],
+    origins=['https://furrylink-frontend.vercel.app']
+    )
 app.secret_key = os.getenv('SECRET_KEY')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')   # configure a database connection to the local file app.db
@@ -19,6 +25,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    # disable modification t
 app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')  
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limit file size to 16 MB  
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}  
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
 app.json.compact = False
 
 metadata = MetaData(naming_convention={
