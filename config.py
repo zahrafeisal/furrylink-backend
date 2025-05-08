7a1ae3bba389dd955ec080c727b642142c960427
zahrafeisal <zahrafeisal3@gmail.com>
2025-05-08 20:02:57 +0300

    credentials configurations

--- config.py
+++ config.py
@@ -5,12 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_bcrypt import Bcrypt
+from flask_session import Session
 from sqlalchemy import MetaData
 from dotenv import load_dotenv    # from python-dotenv library
 
 load_dotenv()   # load env variables
 
 app = Flask(__name__)
+
 CORS(
     app,
     supports_credentials=True,
@@ -18,6 +20,7 @@ CORS(
     allow_headers=["Content-Type", "Accept"],
     origins=['https://furrylink-frontend.vercel.app']
     )
+
 app.secret_key = os.getenv('SECRET_KEY')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')   # configure a database connection to the local file app.db
@@ -27,6 +30,11 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limit file size to 16 MB
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}  
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_TYPE'] = 'filesystem'  # store session data server-side
+app.config['SESSION_PERMANENT'] = False
+
+Session(app)
+
 app.json.compact = False
 
 metadata = MetaData(naming_convention={
